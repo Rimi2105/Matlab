5c2df572c8026f35fcb66ba224b3761f06afdb58
Rimi2105 <140053209+Rimi2105@users.noreply.github.com>
2023-08-21 15:14:36 +0530

    Add files via upload

---
+++ conditional3.m
@@ -0,0 +1,16 @@
+a=5;
+b=6;
+if a>b;
+    fprintf("true")
+else
+    fprintf("false")
+end
+x=6;
+y=8;
+if (x>y);
+c=x*y
+fprintf("product=%d\n",c)
+else 
+    c=x/y
+    fprintf("division=%d\n",c)
+end
